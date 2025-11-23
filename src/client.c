7d21fc5c0449fba0f990ca4e3733ebd769af8f91
zhemepatis <rink.gab@gmail.com>
2025-11-23 19:33:21 +0200

    whitespace

--- src/client.c
+++ src/client.c
@@ -4,7 +4,8 @@ int main()
 {
     // create socket file descriptor
     int server_socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket_desc < 0) {
+    if (server_socket_desc < 0)
+    {
         perror("socket");
         exit(EXIT_FAILURE);
     }
@@ -14,13 +15,15 @@ int main()
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(SERVER_PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0)
+    {
         perror("invalid address");
         exit(EXIT_FAILURE);
     }
 
     // connect to the server
-    if (connect(server_socket_desc, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+    if (connect(server_socket_desc, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
+    {
         perror("connection failed");
         exit(EXIT_FAILURE);
     }
